3b5173153734dafd5191418b430ebccfeb267d48
Rui Figueira <rui.figueira@gmail.com>
2024-09-01 12:55:51 +0100

    fix: make sure .vscode-test/worker-${index} folder exists

--- src/index.ts
+++ src/index.ts
@@ -125,6 +125,7 @@ export const test = base.extend<VSCodeTestFixtures & VSCodeTestOptions & Interna
   _vscodeInstall: [async ({ _createTempDir, vscodeVersion, extensions }, use, workerInfo) => {
     const cachePath = await _createTempDir();
     const installBasePath = path.join(process.cwd(), '.vscode-test', `worker-${workerInfo.workerIndex}`);
+    await fs.promises.mkdir(installBasePath, { recursive: true });
     const installPath = await downloadAndUnzipVSCode({ cachePath: installBasePath, version: vscodeVersion });
     const [cliPath] = resolveCliArgsFromVSCodeExecutablePath(installPath);
 
